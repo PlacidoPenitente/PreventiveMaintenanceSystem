04ade92ecccae02b921b665c05b0450a972bc64a
Jay Mark Estrera <estrerajaymark@ymail.com>
2018-07-23 16:47:03 +0800

    Finished User Model.

---
+++ PreventiveMaintenanceSystem/Models/IModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreventiveMaintenanceSystem.Models
+{
+    public interface IModel
+    {
+        int Id { get; set; }
+
+        DateTime DateAdded { get; set; }
+
+        DateTime DateModified { get; set; }
+    }
+}
